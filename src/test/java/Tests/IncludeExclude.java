8ad9265a5120997cd56fb534362185f7f8fa4cce
dinesh <dheenu02@gmail.com>
2023-09-25 15:09:56 +0530

    Adding Test..

---
+++ src/test/java/Tests/IncludeExclude.java
@@ -0,0 +1,29 @@
+package Tests;
+
+import Pageobject.AdminPage;
+import Pageobject.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.sql.SQLOutput;
+import java.time.Duration;
+
+public class IncludeExclude {
+  @Test
+    public void Test1() {
+      System.out.println("Test1");
+    }
+    @Test
+    public void Test2() {
+        System.out.println("Test2");
+    }
+    @Test
+    public void Test3() {
+        System.out.println("Test3");
+    }
+    @Test
+    public void Test4() {
+        System.out.println("Test3");
+    }
+}
